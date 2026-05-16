57122d6b7deeed507fe3c4659869a6f3965334a1
Alia Lescoulie <alia.lescoulie@gmail.com>
2026-05-16 14:45:34 -0700

    adds basic line control

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,38 @@
 module Main (main) where
 
+import System.Console.ANSI
+import System.IO
+import Control.Monad
+
+writeLine :: String -> IO ()
+writeLine s = do
+  clearLine
+  setCursorColumn 0
+  putStr s
+  hFlush stdout
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  hSetBuffering stdin NoBuffering
+  hSetEcho stdin False
+  loop
+  putStrLn ""
+
+loop :: IO ()
+loop = do
+  key <- getKey
+  case key of
+    "w" -> writeLine "w"
+    "a" -> writeLine "a"
+    "s" -> writeLine "s"
+    "d" -> writeLine "d"
+    _        -> return ()
+  loop
+
+getKey :: IO [Char]
+getKey = reverse <$> getKey' ""
+  where 
+    getKey' chars = do
+      char <- getChar
+      more <- hReady stdin
+      (if more then getKey' else return) (char:chars)
